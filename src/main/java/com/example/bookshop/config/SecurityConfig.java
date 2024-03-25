fead61dd21d24af2da7858c9726a1b0a8e9b398f
Linda-Boateng <linda.boateng@amalitech.com>
2024-03-25 09:26:40 +0000

    done with registration route for users

--- src/main/java/com/example/bookshop/config/SecurityConfig.java
+++ src/main/java/com/example/bookshop/config/SecurityConfig.java
@@ -1,12 +1,46 @@
 package com.example.bookshop.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.cors(
+                cors -> cors.configurationSource(
+                        request -> {
+                            CorsConfiguration config = new CorsConfiguration();
+                            config.setAllowedOrigins(List.of());
+                            config.setAllowedMethods(
+                                    List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
+                            config.setAllowedHeaders(
+                                    List.of("Content-Type", "Content-Disposition", "Authorization"));
+                            return config;
+                        }
+                ))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request-> request
+                .requestMatchers("/api/v1/public/**")
+                .permitAll()
+                .requestMatchers("/api/v1/user/**")
+                .hasAnyAuthority("USER", "ADMIN")
+                .requestMatchers("/api/v1/admin/**")
+                .hasAnyAuthority("ADMIN")
+                .anyRequest()
+                .authenticated())
+                .build();
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
---
+++ src/main/java/com/example/bookshop/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.bookshop.controller;
+
+import com.example.bookshop.dto.UserDto;
+import com.example.bookshop.dto.UserResponseDto;
+import com.example.bookshop.service.userservice.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/public")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserResponseDto> register(@RequestBody UserDto request){
+        return new ResponseEntity<>(userService.registerUser(request), HttpStatus.OK);
+    }
+}
